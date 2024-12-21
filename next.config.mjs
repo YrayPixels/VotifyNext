b3bf622a61e202d2985458317f9ee147d72d99af
YrayPixels <moseserhinyodavwe2@gmail.com>
2024-12-21 08:00:02 +0100

    added the null loader and configured webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.map$/,
+            use: 'null-loader',
+        });
+        return config;
+    },
     reactStrictMode: true,
 };
 
